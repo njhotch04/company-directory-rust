26dfba2a2b0ce7e6933610678aeaa8aaf93db56e
Nathan Hotchkiss <nathanhotchkiss@Mac.lan>
2026-04-07 15:01:11 -0400

    Completed Employee info template. Tested by printing the data to the console

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct Employee {
+    name: String,
+    department: String,
+    email: String,
+    phone: String,
+    remote: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let employee1 = Employee {
+        name: String::from("Nathan Hotchkiss"),
+        department: String::from("Repair"),
+        email: String::from("njhotch04@gmail.com"),
+        phone: String::from("207-XXX-XXXX"),
+        remote: true,
+    };
+
+    println!("Employee Name: {}", employee1.name);
+    println!("Department: {}", employee1.department);
+    println!("Email Address: {}", employee1.email);
+    println!("Phone #: {}", employee1.phone);
+    println!("Remote Employee? {}", employee1.remote);
 }
